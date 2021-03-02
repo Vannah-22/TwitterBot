6118cb22582d0bd3096c008de042d16f281a3d03
Vannah-22 <vannjackson16@gmail.com>
2021-03-01 18:57:02 -0700

    twitter bot, final step; if/elses, loops, response.data === array

--- app.js
+++ app.js
@@ -15,9 +15,23 @@ var params = {
   lang: 'en' // returns only English results //
 }
 
-T.get('search/tweets', params, function(err, data, response) { // GET request, pass through params variable //
-    if(!err){ //IF an arror occurs //
-      // This is where the magic will happen
+T.get('search/tweets', params, function(err, data, response) { // GET request, search tweets using the above params variable//
+    // If there is no error, proceed //
+    if(!err){
+        for(let i = 0; i < data.statuses.length; i++){ // Loop through the returned tweets
+            let id = { id: data.statuses[i].id_str } // Get the tweet Id from the returned data
+            T.post('favorites/create', id, function(err, response){// Try to Favorite the selected Tweet
+            
+                if(err){// If the favorite fails, log the error message
+                console.log(err[0].message);
+                }
+                else {// If the favorite is successful, console.log the url of the tweet //
+                    let username = response.user.screen_name;
+                    let tweetID = response.id_str;
+                    console.log('Favorited:', `https://twitter.com/${username}/status/${tweetId}`)
+                }
+            });
+        }
     } else {
       console.log(err);
     }
